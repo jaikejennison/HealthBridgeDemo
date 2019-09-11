ec590701680b93003de33520010dcc3a4ba30e51
Jaike Howard <jaikejennison@users.noreply.github.com>
2019-09-11 04:55:17 -0400

    Update config.rb

--- JoinTheTeam/TestAutomation/config.rb
+++ JoinTheTeam/TestAutomation/config.rb
@@ -16,11 +16,8 @@ class Config
             yaml = YAML.load(File.read("config/#{page}.yml"))
             @page = Hash.new
             @page[:main_uri] = yaml['mainURI']
-            puts @page[:main_uri]
             @page[:home_uri] = "#{@page[:main_uri]}/home"
-            puts @page[:home_uri]
             @page[:join_the_team_uri] = "#{@page[:main_uri]}/join-the-team"
-            puts @page[:join_the_team_uri]
             @page[:leadership_uri] = "#{@page[:main_uri]}/meet-the-team"
         else
             abort('Please specify a valid yml prefix')  
