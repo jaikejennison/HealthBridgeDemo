0b4fe5bf8764fdb7bae0ec65d952352cc3ca7154
Jaike Howard <jaikejennison@users.noreply.github.com>
2019-09-11 04:02:57 -0400

    Update web_driver.rb

--- JoinTheTeam/TestAutomation/web_driver.rb
+++ JoinTheTeam/TestAutomation/web_driver.rb
@@ -24,15 +24,13 @@ class WebDriver
         @browser_version = capabilities.version
         puts "Using Browser: #{capabilities.browser_name.capitalize} v#{@browser_version}"
       end
-    rescue StandardError => e # Net::ReadTimeout
-      # puts 'Detected Net::ReadTimeout Error, Restarting Browser Session'
-      puts "Detected #{e} Error, Restarting Browser Session"
+    rescue StandardError => e
+      puts "Warn::#{e}. Restarting browser."
       WebDriver.close
       sleep 5
       @read_timeouts += 1
       retry unless @read_timeouts > 2
-      # abort('Maximum Number of Net::ReadTimeout Errors Detected, Aborting')
-      abort("Maximum Number of #{e} Errors Detected, Aborting")
+      abort("ERROR::#{e}. Maximum retry limit reached. Aborting.")
     end
   end
 
