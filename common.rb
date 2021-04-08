b50a2c7f8824246c615a93769cd5de6fc092df2d
Jaike Howard <mentor54@mac.com>
2021-04-08 07:41:18 -0400

    Update common.rb

--- common.rb
+++ common.rb
@@ -5,7 +5,7 @@ class Common
   def self.access_page(uri)
     WebDriver.browser.goto(uri)
     WebDriver.browser.div(:class, 'row-fluid-wrapper').wait_until_present
-    puts "DEBUG::\n\tCurrent URL: #{WebDriver.browser.url}\n\tHTML Dump: #{WebDriver.browser.html}"
+    puts "DEBUG::\n\tCurrent URL: #{WebDriver.browser.url}"
   end
 
   def self.access_form(id, name, text, value, xpath)
@@ -13,11 +13,11 @@ class Common
       puts "DEBUG::\n\tCurrent URL: #{WebDriver.browser.url}\n\tHTML Dump: #{WebDriver.browser.html}"
       form = WebDriver.browser.div(:class, 'cell-wrapper').form(:xpath, "//form[starts-with(@action, #{xpath})]")
       # form = WebDriver.browser.div(:class, 'cell-wrapper').form(:action, xpath)
-      # unless text.nil?
-      #   field = form.field(:name, name)
-      #   field.set(text)
-      # end
-      # form.select_list(:id, id).select(value) unless id.nil? && value.nil?
+      unless text.nil?
+        field = form.field(:name, name)
+        field.set(text)
+      end
+      form.select_list(:id, id).select(value) unless id.nil? && value.nil?
       form.submit
     rescue StandardError => e
       puts "\tERROR::#{e}"
@@ -25,7 +25,7 @@ class Common
   end
 
   def self.click_element(tag, value)
-    puts "DEBUG::\n\tCurrent URL: #{WebDriver.browser.url}\n\tHTML Dump: #{WebDriver.browser.html}"
+    puts "DEBUG::\n\tCurrent URL: #{WebDriver.browser.url}"
     case tag
     when 'span'
       click = WebDriver.browser.span(:class, value)
@@ -33,6 +33,5 @@ class Common
       click = WebDriver.browser.button(:class, value)
     end
     return click.click if click.exists?
-    puts "DEBUG::\n\tCurrent URL: #{WebDriver.browser.url}\n\tHTML Dump: #{WebDriver.browser.html}"
   end
 end
