ff608e490f230bf546ab6557fce2919a160d3e2f
Jaike Howard <mentor54@mac.com>
2021-04-08 04:39:35 -0400

    Update web_driver.rb

--- web_driver.rb
+++ web_driver.rb
@@ -7,16 +7,31 @@ require 'watir'
 class WebDriver
   def self.init(browser)
     @read_timeouts = 0
-    if ENV['HEADLESS']
-      @headless = Headless.new
-      @headless.start
+    begin
+      if ENV['HEADLESS']
+        @headless = Headless.new
+        @headless.start
+      end
+      @browser = Watir::Browser.new browser
+      capabilities = @browser.driver.capabilities
+      @browser_version = capabilities.version
+      puts "Using Browser: #{capabilities.browser_name.capitalize} v#{@browser_version}"
+    rescue StandardError => e
+      puts "Warn::#{e}. Restarting browser."
+      WebDriver.close
+      sleep 5
+      @read_timeouts += 1
+      retry unless @read_timeouts > 2
+      abort("ERROR::#{e}. Maximum retry limit reached. Aborting.")
     end
-    @browser = Watir::Browser.new browser
-    puts "#{@browser.driver.capabilities.browser_name.capitalize} v#{@browser.driver.capabilities.version}"
   end
 
   def self.browser
     attr_reader :browser
+  end
+
+  def self.version
+    attr_reader :browser_version
   end
 
   def self.close
