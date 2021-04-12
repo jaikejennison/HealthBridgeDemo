3395132ac7d0f41a174ae014f558565889458d31
Jaike Howard <mentor54@mac.com>
2021-04-12 03:42:37 -0400

    Update common.rb

--- common.rb
+++ common.rb
@@ -8,13 +8,13 @@ class Common
   end
 
   def self.check_home
-    puts 'DEBUG::LOG::Common.check_home'
     p_class = 'split-hero'
     c_class = "#{p_class}__container"
     d_class = "#{p_class}__details"
     element = WebDriver.browser.div(class: p_class).div(class: c_class).div(class: d_class).h1
-    element.present?
-    puts "TEXT: #{element.text}"
+    # element.present?
+    puts "DEBUG::LOG::Common.check_home: #{element.text}"
+    return element.text.to_s
   end
 
   def self.check_contact
